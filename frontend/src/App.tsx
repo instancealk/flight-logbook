e4db5ff5ab7b8caa2f771383410645e0c1a62d88
picmate <pickmate@gmail.com>
2025-10-14 22:10:46 +0000

    feat: add routes for adding and editing flights in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import FlightsPage from "./pages/FlightsPage";
+import AddFlightPage from "./pages/AddFlightPage";
+import EditFlightPage from "./pages/EditFlightPage";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/flights" element={<FlightsPage />} />
+        <Route path="/flights/new" element={<AddFlightPage />} />
+        <Route path="/flights/edit/:id" element={<EditFlightPage />} />
       </Routes>
     </BrowserRouter>
   );
